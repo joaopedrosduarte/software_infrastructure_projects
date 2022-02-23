26fe37c7d2061617546d6ecef5b5ab3b05e820a0
joaopedrosduarte <jpds@cesar.school>
2022-02-23 18:37:48 -0300

    first reply

--- EX001/main.c
+++ EX001/main.c
@@ -1,38 +1,19 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <stdlib.h>
-#include <sys/wait.h>
+#include <unistd.h>
 
-#ifdef SIMPLES_TEST
-
-int main(){
-    printf("Count: 0\n");
-    fork();
-    printf("Count: 1\n");
-    fork();
-    printf("Count 2\n");
-    fork();
-    printf("Count 3 \n");
-}
-
-#else
-
-int main() {
-    int value = 0;
+int main (){
     pid_t pid;
     pid = fork();
-    if (pid == 0){ //Executando no filho
-        printf("No filho");
-        //execlp("/bin/ls", "ls", "-la", NULL);
-        return 1;
+
+    if (pid == 0){
+        exit(0);
+    } else {
+        printf("Pid of zombie process is : %d \n", pid);
     }
-    wait(&value);
-    printf("No pai %d\n", pid);
-    printf("No pai %d\n", WEXITSTATUS(value));
-    return 0;
+    
+    while (1){
+        sleep(10);
+        return(0);
+    }
 }
-
-    // DOLAR MENOR INDICA DEPENDENCIA OU SEJA  O QUE VAI SER COMPILADO   E DOLAR ARROBA INDICA O ALVO QUE VAI SER CRIADO 
-
-#endif
